8acb84e58e8cb67240b071c22e2f700dbb363e0d
Christoph Kuhn <61102933+Bluewave84@users.noreply.github.com>
2024-10-12 10:50:01 +0200

    Update stepper-motor-test.ino
    jhgj

--- src/stepper-motor-test.ino
+++ src/stepper-motor-test.ino
@@ -1,19 +1,26 @@
 //***Stepper Motor Test Code BY Aaed Musa***
 //-----------------------------------------
+#define EN_PIN           8 // Enable pin
+#define DIR_PIN_1        5 // Direction pin for motor 1
+#define STEP_PIN_1       2 // Step pin for motor 1
+#define DIR_PIN_2        6 // Direction pin for motor 2
+#define STEP_PIN_2       3 // Step pin for motor 2
+#define DIR_PIN_3        7 // Direction pin for motor 3
+#define STEP_PIN_3       4 // Step pin for motor 3
 
 //libraries
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
 //stepper motors
-AccelStepper stepperA(1, 1, 2);  //(driver type, STEP, DIR) Driver A
-AccelStepper stepperB(1, 3, 4);  //(driver type, STEP, DIR) Driver B
-AccelStepper stepperC(1, 5, 6);  //(driver type, STEP, DIR) Driver C
+AccelStepper stepperA(1, STEP_PIN_1, DIR_PIN_1);  //(driver type, STEP, DIR) Driver A
+AccelStepper stepperB(1, STEP_PIN_2, DIR_PIN_2);  //(driver type, STEP, DIR) Driver B
+AccelStepper stepperC(1, STEP_PIN_3, DIR_PIN_3);  //(driver type, STEP, DIR) Driver C
 MultiStepper steppers;           // Create instance of MultiStepper
 
 //stepper motor variables
 int pos[3] = {400, 400, 400};                            // An array to store the target positions for each stepper motor
-int ENA = 0;                           //enable pin for the drivers
+int ENA = EN_PIN;                           //enable pin for the drivers
 
 void setup() {
   //Set iniial maximum speed value for the steppers (steps/sec)
